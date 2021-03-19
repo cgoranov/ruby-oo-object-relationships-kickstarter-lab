3fca87c812c57ee9867e245b7de88c10f8b665fa
Che <cgoranov@gmail.com>
2021-03-19 12:22:18 -0500

    commit tree 8cbf65c7339d956895743eb492e6c9021a222aac

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Backer
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        projects = ProjectBacker.all.select {|project| project.backer == self}
+        projects.map {|x| x.project}
+    end
+
+end
